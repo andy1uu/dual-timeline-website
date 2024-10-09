bee82a6795a0142449b91fa00aad496ed1606299
Andy Luu <andyluu324@gmail.com>
2024-10-09 14:02:11 -0400

    feat: 🎸 Added links to timeline tutorial videos

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -740,7 +740,7 @@ const VideoPlayer = () => {
     <div className="Container mx-auto flex flex-col lg:!w-[1024px] lg:text-sm xl:!w-[1280px] xl:text-xl 2xl:!w-[1536px]">
       <div className="SideBarAndVideoPlayer-container flex h-fit text-primary">
         <div className="SideBar flex h-full flex-col gap-3 bg-dark p-2 lg:!w-[170px] xl:!w-[426px] xl:text-xl 2xl:!w-[256px]">
-          <p className="TimelinesUsedPassword-label mx-auto flex">{timeline.value}</p>
+          <a className="TimelinesUsedPassword-label text-pretty underline" href={timeline.link}>Please watch this tutorial if {timeline.label} is confusing.</a>
           <p className="TimelinesUsedPassword-label mx-auto flex">Password: {timelinesUsedPassword}</p>
           {timeline.value !== "timeline1" && (<div className="EventTypes-container flex flex-col gap-2">
             
--- src/utils/TimelineTypes.js
+++ src/utils/TimelineTypes.js
@@ -1,7 +1,7 @@
 export const timelineTypes = [
-  { key: 1, label: "Timeline 1", value: "timeline1" },
-  { key: 2, label: "Timeline 2", value: "timeline2" },
-  { key: 3, label: "Timeline 3", value: "timeline3" },
-  { key: 4, label: "Timeline 4", value: "timeline4" },
-  { key: 5, label: "Timeline 5", value: "timeline5" },
+  { key: 1, label: "Timeline 1", value: "timeline1", link: "https://youtu.be/1zAtfRO5-Vo" },
+  { key: 2, label: "Timeline 2", value: "timeline2", link: "https://youtu.be/xbqJtAwUq44" },
+  { key: 3, label: "Timeline 3", value: "timeline3", link: "https://youtu.be/5EaFaEGcL0A" },
+  { key: 4, label: "Timeline 4", value: "timeline4", link: "https://youtu.be/TcLQX_mndI8" },
+  { key: 5, label: "Timeline 5", value: "timeline5", link: "https://youtu.be/V_Wz-egSzyw"},
 ];
